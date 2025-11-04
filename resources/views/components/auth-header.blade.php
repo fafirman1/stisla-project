330991d1c178697f4561fe271a53f6ea5cd5b22f
Fajar Firmansyah <87105690+fafirman1@users.noreply.github.com>
2025-11-04 09:39:57 +0700

    posbarserver

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,7 +1,7 @@
  <div class="login-brand">
      <img src="{{ asset('img/logo.png') }}"
          alt="logo"
-         width="100"
+         width="150"
          {{-- class="shadow-light rounded-circle" --}}
          >
  </div>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">Lahminajih Beef</a>
+            <a href="index.html">Rooftop Cafe'</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">MTR</a>
+            <a href="index.html">RCB</a>
         </div>
         <ul class="sidebar-menu">
             <div class="dropdown-divider"></div>
