5ee7beae1bcf9a67775ecb31e1c3ee5806e37dbb
Fajar Firmansyah <87105690+fafirman1@users.noreply.github.com>
2025-11-06 15:40:02 +0700

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2025 <div class="bullet"></div></a> <div class="bullet"></div> Edited By Devani Prasetian
+        Copyright &copy; 2025 <div class="bullet"></div></a> <div class="bullet"></div> Edited By Fajar Firmansyah
     </div>
     <div class="footer-right">
     </div>
